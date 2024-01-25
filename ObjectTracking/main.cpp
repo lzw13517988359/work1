2f3839d5f73022f3e1c814004c2ccfe3fbb8efc1
lzw13517988359 <138019086+lzw13517988359@users.noreply.github.com>
2024-01-25 21:27:38 +0800

    Add files via upload

---
+++ ObjectTracking/main.cpp
@@ -0,0 +1,30 @@
+// Developed by Wei Lisi, tlsirius@163.com
+// July 26th,2017  
+
+// main.cpp : Defines the entry point for the console application.
+#include "tracking.h"
+
+
+int main()
+{
+	Tracking testTracking;
+
+	testTracking.LoadSequence("C:\\ื๗าต2\\ObjectTracking\\kunkunplayball.mp4");
+	while (testTracking.LoadNextFrame() == REPLY_OK && testTracking.GetFrameNum() < 1000)
+	{
+		if (testTracking.GetFrameNum() == 1)
+		{
+			testTracking.CreateResultVideo();
+		}
+		testTracking.ProcessImage();
+		testTracking.DetectTarget();
+		testTracking.TrackTarget();
+		testTracking.DisplayResult(1);
+		testTracking.WriteResultVideo();
+	}
+
+	//testTracking.ReleaseResultVideo();
+	
+	return 0;
+}
+
